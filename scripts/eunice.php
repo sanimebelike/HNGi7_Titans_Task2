f22a52365d678263b3d50709a1993c51386a21b8
sanimebelike <66335375+sanimebelike@users.noreply.github.com>
2020-06-07 14:10:43 +0100

    Eunice.php
    Eunice ebelike's test

---
+++ scripts/eunice.php
@@ -0,0 +1,12 @@
+<?php
+
+$myName = "Eunice Ebelike";
+$myID = "HNG-06015";
+$myLanguage = "PHP";
+$myEmail = "sanimebelike@gmail.com";
+
+
+echo "Hello World, this is $myName with HNGi7 ID $myID  and Email $myEmail using $myLanguage for stage 2 task";
+
+?>
+
