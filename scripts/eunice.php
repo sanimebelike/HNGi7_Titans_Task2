f3d8d6b7a189845f91351eb5ad922d79a351939e
sanimebelike <66335375+sanimebelike@users.noreply.github.com>
2020-06-07 17:08:04 +0100

    Update eunice.php

--- scripts/eunice.php
+++ scripts/eunice.php
@@ -1,11 +1,11 @@
 <?php
 
 $Name = "Eunice Ebelike";
-$ID = "HNG-06015";
+$Id = "HNG-06015";
 $Email = "sanimebelike@gmail.com";
 $Language = "PHP";
 
-echo "Hello World, this is $Name with HNGi7 ID $ID using $Language for stage 2 task";
+echo "Hello World, this is $Name with HNGi7 ID $Id using $Language for stage 2 task";
 
 ?>
 
